ae2d4fcec1f610bfc1f6d859b0d7f9603ecab239
Rahul Jindal <rahuljindal1803@gmail.com>
2024-01-28 02:51:26 +0530

    -feat -- login & signup page

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,55 @@
 import React from "react";
-
+import LandImg from "../../assets/NK_login.jpeg";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div className="w-full min-h-screen font-sans text-gray-800">
+        <div className="px-6 mx-auto max-w-7xl ">
+          <div className="flex justify-between items-center mt-2 p-18 ">
+            <h1 className="font-medium text-3xl ">Notes.io</h1>
+            <nav className="flex  items-center space-x-10">
+              <button
+                className="h-10 w-24 bg-blue-600 text-white rounded-2xl cursor-pointer hover:bg-blue-500 hover:scale-110 transition-all active:bg-blue-700 shadow-lg shadow-slate-200"
+                onClick={() => {
+                  navigate("/login");
+                }}
+              >
+                Login
+              </button>
+            </nav>
+          </div>
+          <div className="px-6 mx-auto max-w-6xl">
+            <div className="flex">
+              <div className="pt-24 pr-8 w-1/2">
+                <h1 className="text-6xl font-bold font-display">
+                  "Note.io - Capture Ideas, Stay Organized"
+                </h1>
+                <p className="pt-8 max-w-md text-xl leading-relaxed text-gray-500">
+                  A simple and powerful notes app for organizing your thoughts.
+                </p>
+                <div className="flex  items-center pt-6 space-x-6">
+                  <button
+                    onClick={() => {
+                      navigate("/register");
+                    }}
+                    className="w-28 h-10 bg-blue-600 text-white rounded-2xl cursor-pointer hover:bg-blue-500 hover:scale-110 transition-all active:bg-blue-700 shadow-xl shadow-gray-300 "
+                  >
+                    Get Started{" "}
+                  </button>
+                </div>
+              </div>
+              <div className="mx-4 px-5">
+                <img className="mx-2" src={LandImg} alt="logo" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,7 +1,65 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import avatar from "../../assets/profile.png";
+import styles from "../../styles/Login.module.css";
 const Login = () => {
-  return <div>Login</div>;
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  return (
+    <div>
+      <div className="container mx-auto flex justify-center ">
+        <div className=" w-5/12 flex justify-center items-center h-screen ">
+          <div className="bg-[#ffffff8c] rounded-2xl shadow-2xl w-8/12 backdrop-blur-md border-1 border-gray-50 h-3/4  pt-4 ">
+            <div className="title flex flex-col  items-center">
+              <h4 className="text-5xl font-bold">Login!</h4>
+              <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+                Your Personal Space for Ideas and Thoughts
+              </span>
+            </div>
+            <form action="/user/login" method="post" className="py-1">
+              <div className="profile flex justify-center py-4">
+                <img src={avatar} className={styles.profile_img} alt="" />
+              </div>
+              <div className=" flex flex-col justify-center items-center gap-6">
+                <input
+                  type="text"
+                  id="name"
+                  value={user.email}
+                  className={styles.textbox}
+                  placeholder="Username"
+                  on
+                />
+                <input
+                  type="text"
+                  value={user.password}
+                  className={styles.textbox}
+                  placeholder="password"
+                />
+
+                <button
+                  type="submit"
+                  className="border box-border bg-indigo-500 hover:bg-[#ff6a6a] transition-all w-3/4 py-4 rounded-lg text-gray-50 text-xl shadow-sm text-center"
+                >
+                  Let's Go
+                </button>
+              </div>
+              <div className="text-center py-4">
+                <span className="text-gray-500 ">
+                  Don't have an account?{" "}
+                  <Link className="text-red-500" to="/signup">
+                    Register Now
+                  </Link>
+                </span>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
